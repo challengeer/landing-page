ae523dcb96eb9f5d127dc1a5c93e168bfc686805
mark-takac <steam.omg.mk@gmail.com>
2025-03-16 18:07:59 +0100

    Change images aspect ratio

--- app/page.tsx
+++ app/page.tsx
@@ -167,7 +167,7 @@ export default function Home() {
               <CarouselContent className="-ml-2 md:-ml-4">
                 {communityImages.map((image, index) => (
                   <CarouselItem key={index} className="pl-2 md:pl-4 basis-1/2 sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5">
-                    <div className="relative aspect-[3/4] w-full overflow-hidden">
+                    <div className="relative aspect-[2/3] w-full overflow-hidden">
                       <Image
                         src={image.src}
                         alt={image.alt}
