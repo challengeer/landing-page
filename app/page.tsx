9861bcc34d08abe3ee21ec0eb080655275af6080
mark-takac <steam.omg.mk@gmail.com>
2025-03-16 12:20:26 +0100

    Forgot to delete a line

--- app/page.tsx
+++ app/page.tsx
@@ -266,7 +266,6 @@ export default function Home() {
       <footer className="border-t py-6 md:py-0">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row px-4 md:px-6">
           <div className="flex items-center gap-2">
-            {/* <TrophyIcon className="h-5 w-5 text-primary" /> */}
             <Image src="/icon.png" alt="Challengeer Logo" width={32} height={32} className="object-contain rounded-md" />
             <p className="text-sm font-medium">© {new Date().getFullYear()} Challengeer. All rights reserved.</p>
           </div>
