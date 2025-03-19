51132bff6789c177cb1d95f3f91d8d74bd6a2d90
mark-takac <steam.omg.mk@gmail.com>
2025-03-19 23:04:57 +0100

    Changed colors for dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -79,10 +79,10 @@ export default function Home() {
   }, [rerender, xTranslation, duration, width]);
 
   return (
-    <div className="flex min-h-screen flex-col dark:bg-slate-900">
+    <div className="flex min-h-screen flex-col dark:bg-neutral-900">
 
       {/* Header - change this later */}
-      <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:bg-slate-800/95 dark:supports-[backdrop-filter]:bg-slate-800/60">
+      <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:bg-neutral-800/95 dark:supports-[backdrop-filter]:bg-neutral-800/60">
         <div className="max-w-screen-xl mx-auto px-6 md:px-10 py-4">
           <div className="flex items-center justify-between relative">
             {/* Logo */}
@@ -232,7 +232,7 @@ export default function Home() {
 
         {/* Gradient section */}
         <section className="py-24 md:py-36 relative overflow-hidden">
-          <div className="absolute inset-0 bg-gradient-to-br from-purple-500 via-fuchsia-500 to-pink-500 z-0"></div>
+          <div className="absolute inset-0 bg-gradient-to-br from-purple-500 via-fuchsia-500 to-pink-500 z-0 dark:from-purple-800 dark:via-fuchsia-700 dark:to-pink-800"></div>
           <div className="max-w-screen-xl mx-auto relative z-10 px-6 md:px-10 text-left">
             <h2 className="text-4xl sm:text-5xl lg:text-6xl font-bold leading-snug tracking-tight text-white md:text-left">
               Something that with
