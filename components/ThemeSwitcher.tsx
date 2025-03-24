d2e2996f3288a730714bd9bc89dae36aa7c76e35
mark-takac <steam.omg.mk@gmail.com>
2025-03-24 19:57:16 +0100

    ThemeSwitcher component for toggling light/dark mode

---
+++ components/ThemeSwitcher.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { Button } from "@/components/ui/button";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
+import { useTheme } from "next-themes";
+import { useMounted } from "@/hooks/useMounted";
+
+export function ThemeSwitcher() {
+  const { theme, setTheme } = useTheme();
+  const mounted = useMounted();
+
+  if (!mounted) return null;
+
+  return (
+    <Button
+      variant="ghost"
+      size="default"
+      className="flex items-center gap-2 h-8 hover:text-primary"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark" ? (
+        <SunIcon className="h-6 w-6" />
+      ) : (
+        <MoonIcon className="h-6 w-6" />
+      )}
+    </Button>
+  );
+} 
